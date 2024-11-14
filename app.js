f100c2d6f6a2be6ecfd3f03bf88e259dd9e45971
Alan Sanchez <sanchez1.alan1@gmail.com>
2024-11-14 15:56:09 -0700

    Update app.js

--- app.js
+++ app.js
@@ -304,7 +304,7 @@ function calculateScore(hand) {
     return score;
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
