bb6949aa4013f8b53bc21974936306d6e654b1df
DanielFreeman068 <141285622+DanielFreeman068@users.noreply.github.com>
2025-02-17 22:34:13 -0700

    fix home

--- app.js
+++ app.js
@@ -306,5 +306,5 @@ function calculateScore(hand) {
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
